6034aa6b9070396e4ce00c6771b9f466454b667f
Sofarz <72409381+Sofarz@users.noreply.github.com>
2021-02-08 20:31:18 +0100

    Setup DOM selectors in top. Added eventListerners in bottom for FIND OUT button and for choosing question. Started setting currentQuestion object and set up value variable. Compared currentQuestion with secret person under checkQuestion and invoked filterCharacters.

--- code/script.js
+++ code/script.js
@@ -2,6 +2,9 @@
 const board = document.getElementById('board')
 const questions = document.getElementById('questions')
 const restartButton = document.getElementById('restart')
+const filterBtn = document.getElementById('filter');
+const winOrLose = document.getElementById('winOrLose');
+const playAgainBtn = document.getElementById('playAgain');
 
 // Array with all the characters, as objects
 const CHARACTERS = [
@@ -226,6 +229,7 @@ const CHARACTERS = [
 
 // Global variables
 let secret, currentQuestion, charactersInPlay
+let value;
 
 // Draw the game board
 const generateBoard = () => {
@@ -254,8 +258,9 @@ const start = () => {
   // Here we're setting charactersInPlay array to be all the characters to start with
   charactersInPlay = CHARACTERS
   // What else should happen when we start the game?
-  setSecret()
+  
   generateBoard()
+  setSecret()
 }
 
 // setting the currentQuestion object when you select something in the dropdown
@@ -263,39 +268,53 @@ const selectQuestion = () => {
   const category = questions.options[questions.selectedIndex].parentNode.label
   // This variable stores what option group (category) the question belongs to.
   // We also need a variable that stores the actual value of the question we've selected.
-
+  value = questions.value;
   if (category === 'hair color') {
     currentQuestion = {
       attribute: 'hairColor',
-      // value: ,
+      value: value,
       // 👆 add the value from the input here
       category: category,
     }
   } else if (category === 'eye color') {
     // Set this up your self
+    currentQuestion = {
+      attribute: 'eyeColor',
+      value: value,
+      // 👆 add the value from the input here
+      category: category,
+    }
   } else if (category === 'accessories') {
     currentQuestion = {
-      //attribute: ,
+      attribute: value,
       // 👆 this is the property of the booleans such as smoke, glasses and hat. add the value from the input here
       value: true, // we're asking if this person wears a hat for exaple, so always true in the question.
       category: category,
     }
   } else if (category === 'other') {
-    // Set this up your self (should be same structure as above)
+    currentQuestion = {
+      attribute: 'smoker',
+      value: true, // or value?
+      // 👆 add the value from the input here
+      category: category,
+    }// Set this up your self (should be same structure as above)
+    
   }
 }
 
 // This function should be invoked when you click on 'Find Out'.
 const checkQuestion = () => {
+  const keep = (currentQuestion.value === secret[currentQuestion.value])
   // Compare the currentQuestion with the secret person.
   // See if we should keep or remove people based on that
   // Then invoke filterCharacters
+  filterCharacters(keep)
 }
 
 // It'll filter the characters array and redraw the game board.
 const filterCharacters = (keep) => {
   // Show the correct alert message for different categories
-  if (group === 'accessories') {
+  if (category === 'accessories') {
     if (keep) {
       alert(
         `Yes, the person wears ${attribute}! Keep all that wears ${attribute}`
@@ -305,7 +324,7 @@ const filterCharacters = (keep) => {
         `No, the person doesn't wear ${attribute}! Remove all that wears ${attribute}`
       )
     }
-  } else if (group === 'other') {
+  } else if (category === 'other') {
     // Similar to the one above
   } else {
     if (keep) {
@@ -343,3 +362,6 @@ start()
 
 // All the event listeners
 restartButton.addEventListener('click', start)
+questions.addEventListener('change', () => selectQuestion(questions.value))
+filterBtn.addEventListener('click', checkQuestion)
+playAgainBtn.addEventListener('click', start)

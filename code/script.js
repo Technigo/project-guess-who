8aac504404c92106688cf1d74c55bbd012bd7881
Maria Bergman <maria.bergman@elektroskandia.se>
2021-02-10 16:33:03 +0100

    Generated board and added more options to choose from

--- code/script.js
+++ code/script.js
@@ -247,6 +247,7 @@ const generateBoard = () => {
 // Randomly select a person from the characters array and set as the value of the variable called secret
 const setSecret = () => {
   secret = charactersInPlay[Math.floor(Math.random() * charactersInPlay.length)]
+  console.log(secret);
 }
 
 // This function to start (and restart) the game
@@ -254,7 +255,9 @@ const start = () => {
   // Here we're setting charactersInPlay array to be all the characters to start with
   charactersInPlay = CHARACTERS
   // What else should happen when we start the game?
-}
+  generateBoard();
+  setSecret;
+};
 
 // setting the currentQuestion object when you select something in the dropdown
 const selectQuestion = () => {

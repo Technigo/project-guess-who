462ba0018eb2529632baa1433e1bdafadf814bcf
Sara Carlstein <carlstein.sara@gmail.com>
2021-02-10 14:37:16 +0100

    changes

--- code/script.js
+++ code/script.js
@@ -263,7 +263,6 @@ const selectQuestion = () => {
   const category = questions.options[questions.selectedIndex].parentNode.label
   // This variable stores what option group (category) the question belongs to.
   // We also need a variable that stores the actual value of the question we've selected.
-  console.log('bajs')
   if (category === 'hair color') {
     currentQuestion = {
       attribute: 'hairColor',

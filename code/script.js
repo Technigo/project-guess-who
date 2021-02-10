2162e4371263e40bec0d7fac4d33f8a1d3f358c7
DANIELA <DANIELA.ZACARIAS@OUTLOOK.COM>
2021-02-09 23:50:21 -0600

    Step 2: Selecting the secret perso when the game starts

--- code/script.js
+++ code/script.js
@@ -256,8 +256,12 @@ const start = () => {
   // Here we're setting charactersInPlay array to be all the characters to start with
   charactersInPlay = CHARACTERS
   // What else should happen when we start the game?
+
+  //Displaying the board so we can start the game
   generateBoard();
-  
+  // When the games starts the secret person will be selected
+  setSecret();
+   
 }
 
 // setting the currentQuestion object when you select something in the dropdown

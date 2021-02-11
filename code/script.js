d369bbc8be2899136f9d5cee96ca985a940ee2bf
Pauan8 <pauline.j.andersson@vgregion.se>
2021-02-11 17:24:57 +0100

    spelling

--- code/script.js
+++ code/script.js
@@ -420,7 +420,7 @@ const selectQuestion = (handleOption) => {
 const checkQuestion = () => {
   let group;
 
-  currentQuestion.category === ("accessories" || "behavior") ? (
+  currentQuestion.category === "accessories" || currentQuestion.category === "behaviour" ? (
     group = currentQuestion.attribute
   ) : (
     group = currentQuestion.value

5ff54764a1b35a489a41ee4e90a64d28910d07a0
madeleinesvensson <56533906+madeleinesvensson@users.noreply.github.com>
2021-09-15 23:18:18 +0200

    added imgs to cards, logic and categories/values added to objects

--- code/script.js
+++ code/script.js
@@ -1,213 +1,239 @@
 // All the DOM selectors stored as short variables
-const board = document.getElementById('board')
-const questions = document.getElementById('questions')
-const restartButton = document.getElementById('restart')
+const board = document.getElementById("board");
+const questions = document.getElementById("questions");
+const restartButton = document.getElementById("restart");
+const filterButton = document.getElementById("filter");
+const restart = document.getElementById("playAgain");
 
 // Array with all the characters, as objects
 const CHARACTERS = [
   {
-    name: 'Jabala',
-    img: 'images/jabala.svg',
-    hair: 'hidden',
-    eyes: 'hidden',
-    accessories: ['glasses', 'hat'],
-    other: []
+    name: "Harry Potter",
+    img: "images/harry.png",
+    hair: "black",
+    house: "gryffindor",
+    accessories: ["glasses", "pet", "wand"],
+    student: "yes",
+    aWeasley: "no",
   },
   {
-    name: 'Jack',
-    img: 'images/jack.svg',
-    hair: 'hidden',
-    eyes: 'blue',
-    accessories: ['hat'],
-    other: []
+    name: "Hermione Granger",
+    img: "images/hermione.webP",
+    hair: "red",
+    house: "gryffindor",
+    accessories: ["pet", "wand"],
+    student: "yes",
+    aWeasley: "no",
   },
   {
-    name: 'Jacques',
-    img: 'images/jacques.svg',
-    hair: 'grey',
-    eyes: 'blue',
-    accessories: ['hat'],
-    other: ['smoker']
+    name: "Ron Weasley",
+    img: "images/ron.jpg",
+    hair: "red",
+    house: "gryffindor",
+    accessories: ["pet", "wand"],
+    student: "yes",
+    aWeasley: "yes",
   },
   {
-    name: 'Jai',
-    img: 'images/jai.svg',
-    hair: 'black',
-    eyes: 'brown',
-    accessories: [],
-    other: []
+    name: "Ginny Weasley",
+    img: "images/ginny.jpg",
+    hair: "red",
+    house: "gryffindor",
+    accessories: ["wand"],
+    student: "yes",
+    aWeasley: "yes",
   },
   {
-    name: 'Jake',
-    img: 'images/jake.svg',
-    hair: 'yellow',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: "Molly Weasley",
+    img: "images/molly.jpg",
+    hair: "red",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "no",
+    aWeasley: "yes",
   },
   {
-    name: 'James',
-    img: 'images/james.svg',
-    hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: "Minerva McGonagall",
+    img: "images/minerva.webP",
+    hair: "gray",
+    house: "noHouse",
+    accessories: ["hat", "wand"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Jana',
-    img: 'images/jana.svg',
-    hair: 'black',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: []
+    name: "Dolores Umbridge",
+    img: "images/dolores.jpg",
+    hair: "brown",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Jane',
-    img: 'images/jane.svg',
-    hair: 'yellow',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: []
+    name: "Luna Lovegood",
+    img: "images/luna.jpg",
+    hair: "blonde",
+    house: "ravenclaw",
+    accessories: ["wand"],
+    student: "yes",
+    aWeasley: "no",
   },
   {
-    name: 'Jaqueline',
-    img: 'images/jaqueline.svg',
-    hair: 'orange',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: "Rubeus Hargrid",
+    img: "images/hagrid.webP",
+    hair: "brown",
+    house: "noHouse",
+    accessories: ["pet"],
+    student: "no",
+    aWeasley: "no",
   },
 
   {
-    name: 'Jazebelle',
-    img: 'images/jazebelle.svg',
-    hair: 'purple',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: ['smoker']
+    name: "Fred Weasley",
+    img: "images/fred.jpg",
+    hair: "red",
+    house: "gryffindor",
+    accessories: ["wand"],
+    student: "yes",
+    aWeasley: "yes",
   },
   {
-    name: 'Jean',
-    img: 'images/jean.svg',
-    hair: 'brown',
-    eyes: 'blue',
-    accessories: ['glasses', 'hat'],
-    other: ['smoker']
+    name: "George Weasley",
+    img: "images/george.jpg",
+    hair: "red",
+    house: "gryffindor",
+    accessories: ["wand"],
+    student: "yes",
+    aWeasley: "yes",
   },
   {
-    name: 'Jeane',
-    img: 'images/jeane.svg',
-    hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: "Bellatrix Lestrange",
+    img: "images/bella.jpg",
+    hair: "black",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Jed',
-    img: 'images/jed.svg',
-    hair: 'orange',
-    eyes: 'green',
-    accessories: ['glasses', 'hat'],
-    other: ['smoker']
+    name: "Draco Malfoy",
+    img: "images/draco.jpg",
+    hair: "blonde",
+    house: "slytherin",
+    accessories: ["wand"],
+    student: "yes",
+    aWeasley: "no",
   },
   {
-    name: 'Jenni',
-    img: 'images/jenni.svg',
-    hair: 'white',
-    eyes: 'hidden',
-    accessories: ['hat'],
-    other: []
+    name: "Sirius Black",
+    img: "images/sirius.jpg",
+    hair: "brown",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Jeri',
-    img: 'images/jeri.svg',
-    hair: 'orange',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: "Voldemort",
+    img: "images/voldemort.webP",
+    hair: "none",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Jerry',
-    img: 'images/jerry.svg',
-    hair: 'hidden',
-    eyes: 'blue',
-    accessories: ['hat'],
-    other: []
+    name: "Neville Longbottom",
+    img: "images/neville.jpg",
+    hair: "brown",
+    house: "gryffindor",
+    accessories: ["wand"],
+    student: "yes",
+    aWeasley: "no",
   },
   {
-    name: 'Jess',
-    img: 'images/jess.svg',
-    hair: 'black',
-    eyes: 'blue',
-    accessories: ['glasses'],
-    other: []
+    name: "Dumbledore",
+    img: "images/dumbledore.webP",
+    hair: "grey",
+    house: "noHouse",
+    accessories: ["wand", "hat"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Jocelyn',
-    img: 'images/jocelyn.svg',
-    hair: 'black',
-    eyes: 'brown',
-    accessories: ['glasses'],
-    other: []
+    name: "Severus Snape",
+    img: "images/snape.jpg",
+    hair: "black",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Jon',
-    img: 'images/jon.svg',
-    hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: "Dobby",
+    img: "images/dobby.jpg",
+    hair: "none",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Jordan',
-    img: 'images/jordan.svg',
-    hair: 'yellow',
-    eyes: 'hidden',
-    accessories: ['glasses', 'hat'],
-    other: []
+    name: "Remus Lupin",
+    img: "images/lupin.jpg",
+    hair: "red",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "no",
+    aWeasley: "no",
   },
   {
-    name: 'Josephine',
-    img: 'images/josephine.svg',
-    hair: 'grey',
-    eyes: 'brown',
+    name: "Peter Pettigrew",
+    img: "images/peter.jpg",
+    hair: "blonde",
+    house: "noHouse",
+    accessories: ["wand"],
+    student: "yes",
+    aWeasley: "no",
+  },
+  {
+    name: "Seamus Finnigan",
+    img: "images/seamus.jpg",
+    hair: "brown",
+    house: "gryffindor",
+    accessories: ["wand"],
+    student: "yes",
+    aWeasley: "no",
+  },
+  {
+    name: "Dudley Dursley",
+    img: "images/dudley.jpg",
+    hair: "brown",
+    house: "noHouse",
     accessories: [],
-    other: []
+    student: "yes",
+    aWeasley: "no",
   },
   {
-    name: 'Josh',
-    img: 'images/josh.svg',
-    hair: 'yellow',
-    eyes: 'green',
-    accessories: [],
-    other: []
+    name: "Argus Filch",
+    img: "images/filch.jpg",
+    hair: "gray",
+    house: "noHouse",
+    accessories: ["pet"],
+    student: "no",
+    aWeasley: "no",
   },
-  {
-    name: 'Jude',
-    img: 'images/jude.svg',
-    hair: 'black',
-    eyes: 'green',
-    accessories: [],
-    other: []
-  },
-  {
-    name: 'Julie',
-    img: 'images/julie.svg',
-    hair: 'black',
-    eyes: 'brown',
-    accessories: ['glasses', 'hat'],
-    other: []
-  },
-]
+];
 
 // Global variables
-let secret
-let currentQuestion
-let charactersInPlay
+let secret;
+let currentQuestion;
+let charactersInPlay;
 
 // Draw the game board
 const generateBoard = () => {
-  board.innerHTML = ''
+  board.innerHTML = "";
   charactersInPlay.forEach((person) => {
     board.innerHTML += `
       <div class="card">
@@ -218,74 +244,154 @@ const generateBoard = () => {
           <button class="filled-button small" onclick="guess('${person.name}')">Guess</button>
         </div>
       </div>
-    `
-  })
-}
+    `;
+  });
+};
 
 // Randomly select a person from the characters array and set as the value of the variable called secret
 const setSecret = () => {
-  secret = charactersInPlay[Math.floor(Math.random() * charactersInPlay.length)]
-}
+  secret =
+    charactersInPlay[Math.floor(Math.random() * charactersInPlay.length)];
+  console.log(secret);
+};
 
 // This function to start (and restart) the game
 const start = () => {
   // Here we're setting charactersInPlay array to be all the characters to start with
-  charactersInPlay = CHARACTERS
+  charactersInPlay = CHARACTERS;
+  setSecret();
   // What else should happen when we start the game?
-}
+  generateBoard(charactersInPlay);
+};
 
 // setting the currentQuestion object when you select something in the dropdown
 const selectQuestion = () => {
-  const category = questions.options[questions.selectedIndex].parentNode.label
+  const category = questions.options[questions.selectedIndex].parentNode.label;
 
   // This variable stores what option group (category) the question belongs to.
   // We also need a variable that stores the actual value of the question we've selected.
-  // const value =
+  const value = questions.value;
 
   currentQuestion = {
     category: category,
-    // value: value
-  }
-}
-
+    value: value,
+  };
+};
+selectQuestion();
 // This function should be invoked when you click on 'Find Out' button.
 const checkQuestion = () => {
-  const { category, value } = currentQuestion
-
+  const { category, value } = currentQuestion;
+  let keep;
   // Compare the currentQuestion details with the secret person details in a different manner based on category (hair/eyes or accessories/others).
   // See if we should keep or remove people based on that
   // Then invoke filterCharacters
-  if (category === 'hair' || category === 'eyes') {
-
-  } else if (category === 'accessories' || category === 'other') {
-
+  if (category === "hair") {
+    keep = secret.hair === value;
+  } else if (category === "house") {
+    keep = secret.house === value;
+  } else if (category === "accessories") {
+    keep = secret.accessories.includes(value);
+  } else if (category === "student") {
+    keep = secret.student === value;
+  } else if (category === "aWeasley") {
+    keep = secret.aWeasley === value;
   }
-}
+  filterCharacters(keep);
+};
 
 // It'll filter the characters array and redraw the game board.
 const filterCharacters = (keep) => {
-  const { category, value } = currentQuestion
+  const { category, value } = currentQuestion;
   // Show the correct alert message for different categories
-  if (category === 'accessories') {
+  if (category === "accessories") {
     if (keep) {
       alert(
-        `Yes, the person wears ${value}! Keep all people that wears ${value}`
-      )
+        `Yes, the person have a ${value}! Keep all people that have a ${value}`
+      );
+      charactersInPlay = charactersInPlay.filter((person) =>
+        person[category].includes(value)
+      );
     } else {
       alert(
-        `No, the person doesn't wear ${value}! Remove all people that wears ${value}`
-      )
+        `No, the person doesn't have a ${value}! Remove all people that have a ${value}`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => !person[category].includes(value)
+      );
     }
-  } else if (category === 'other') {
-    // Similar to the one above
-  } else {
+  } else if (category === "hair") {
     if (keep) {
-      // alert popup that says something like: "Yes, the person has yellow hair! Keep all people with yellow hair"
+      alert(
+        `Yes, the person have ${value} hair! Keep all people that have ${value} hair`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => person[category] === value
+      );
     } else {
-      // alert popup that says something like: "No, the person doesnt have yellow hair! Remove all people with yellow hair"
+      alert(
+        `No, the person doesn't have ${value} hair! Remove all people that have ${value} hair`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => person[category] !== value
+      );
+    }
+  } else if (category === "house") {
+    if (keep) {
+      alert(
+        value === "noHouse"
+          ? "Yes, the person does not live in a house! Kepp all the people that does not belong to a house"
+          : `Yes, the person belongs to ${value}! Keep all people that belongs to ${value}`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => person[category] === value
+      );
+    } else {
+      alert(
+        value === "noHouse"
+          ? "No, the person lives to a house! Removes all people that doesn't live in a house"
+          : `No, the person doesn't belong to ${value}! Remove all people that belongs to${value}`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => person[category] !== value
+      );
+    }
+  } else if (category === "student") {
+    if (keep) {
+      alert(
+        `Yes, the person is a ${value}! Keep all people that is a ${value}`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => person[category] === value
+      );
+    } else {
+      alert(
+        `No, the person isn't a student! Remove all people that is a student`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => person[category] !== value
+      );
+    }
+  } else if (category === "aWeasley") {
+    if (keep) {
+      alert(
+        `Yes, the person is related to a Weasley! Keep all people that is related to a Weasley`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => person[category] === value
+      );
+      console.log(category, value);
+    } else {
+      alert(
+        `No, the person isn't related to a Weasley! Remove all people that is related to a Weasley`
+      );
+      charactersInPlay = charactersInPlay.filter(
+        (person) => person[category] !== value
+      );
+      console.log(category, value);
     }
   }
 
+  generateBoard();
   // Determine what is the category
   // filter by category to keep or remove based on the keep variable.
   /* 
@@ -301,25 +407,43 @@ const filterCharacters = (keep) => {
   */
 
   // Invoke a function to redraw the board with the remaining people.
-}
+};
 
 // when clicking guess, the player first have to confirm that they want to make a guess.
-const guess = (personToConfirm) => {
+const guess = (personToCheck) => {
+  //const guessedName = charactersInPlay.name;
+  confirm(`You are about to guess ${personToCheck}?`);
   // store the interaction from the player in a variable.
   // remember the confirm() ?
   // If the player wants to guess, invoke the checkMyGuess function.
-}
+
+  checkMyGuess(personToCheck);
+};
 
 // If you confirm, this function is invoked
 const checkMyGuess = (personToCheck) => {
+  if (personToCheck === secret.name) {
+    winOrLoseText.innerHTML = `You have won`;
+  } else {
+    winOrLoseText.innerHTML = `You have lost`;
+  }
   // 1. Check if the personToCheck is the same as the secret person's name
   // 2. Set a Message to show in the win or lose section accordingly
   // 3. Show the win or lose section
   // 4. Hide the game board
-}
+  winOrLose.style.display = "flex";
+};
 
 // Invokes the start function when website is loaded
-start()
+start();
 
 // All the event listeners
-restartButton.addEventListener('click', start)
+restartButton.addEventListener("click", start);
+questions.addEventListener("change", selectQuestion);
+filterButton.addEventListener("click", checkQuestion);
+restart.addEventListener("click", () => {
+  start();
+  winOrLose.style.display = "none";
+});
+
+/* GLÖM INTE TA DISPLAY NONE EFTER VINST*/

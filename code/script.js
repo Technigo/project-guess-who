3c18abb31db956a8da91ab21500cb4de466c1820
Lovisa Carling <lovisa.carling@gmail.com>
2021-09-15 23:55:32 +0200

    Fixed bugg in filterCharacters function and changed characters to lol champions with other categories

--- code/script.js
+++ code/script.js
@@ -9,197 +9,341 @@ const playAgainButton = document.getElementById('playAgain')
 // Array with all the characters, as objects
 const CHARACTERS = [
   {
-    name: 'Jabala',
-    img: 'images/jabala.svg',
-    hair: 'hidden',
-    eyes: 'hidden',
-    accessories: ['glasses', 'hat'],
-    other: []
-  },
-  {
-    name: 'Jack',
-    img: 'images/jack.svg',
-    hair: 'hidden',
-    eyes: 'blue',
-    accessories: ['hat'],
-    other: []
-  },
-  {
-    name: 'Jacques',
-    img: 'images/jacques.svg',
-    hair: 'grey',
-    eyes: 'blue',
-    accessories: ['hat'],
-    other: ['smoker']
-  },
-  {
-    name: 'Jai',
-    img: 'images/jai.svg',
+    name: 'Ahri',
+    img: 'champs/Ahri.jpg',
     hair: 'black',
-    eyes: 'brown',
-    accessories: [],
-    other: []
+    costume: 'red',
+    abilities: ['ability power'],
+    crowdcontrol: ['charm']
   },
   {
-    name: 'Jake',
-    img: 'images/jake.svg',
-    hair: 'yellow',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: 'Anivia',
+    img: 'champs/Anivia.jpg',
+    hair: 'none',
+    costume: 'blue',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['stunn']
   },
   {
-    name: 'James',
-    img: 'images/james.svg',
+    name: 'Cassiopeia',
+    img: 'champs/Cassiopeia.jpg',
+    hair: 'hidden',
+    costume: 'green',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['grounded', 'stunn']
+  },
+  {
+    name: 'Galio',
+    img: 'champs/Galio.jpg',
+    hair: 'none',
+    costume: 'white',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['taunt', 'knock up']
+  },
+  {
+    name: 'Gragas',
+    img: 'champs/Gragas.jpg',
+    hair: 'red',
+    costume: 'brown',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['stunn', 'knock back']
+  },
+  {
+    name: 'Karma',
+    img: 'champs/Karma.jpg',
     hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    costume: 'purple',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['root']
   },
   {
-    name: 'Jana',
-    img: 'images/jana.svg',
+    name: 'LeBlanc',
+    img: 'champs/LeBlanc.jpg',
     hair: 'black',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: []
+    costume: 'purple',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['root']
   },
   {
-    name: 'Jane',
-    img: 'images/jane.svg',
-    hair: 'yellow',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: []
+    name: 'Malzahar',
+    img: 'champs/Malzahar.jpg',
+    hair: 'hidden',
+    costume: 'purple',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['silence', 'suppress']
   },
   {
-    name: 'Jaqueline',
-    img: 'images/jaqueline.svg',
-    hair: 'orange',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: 'Neeko',
+    img: 'champs/Neeko.jpg',
+    hair: 'purple',
+    costume: 'brown',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['root', 'stunn']
   },
 
   {
-    name: 'Jazebelle',
-    img: 'images/jazebelle.svg',
-    hair: 'purple',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: ['smoker']
+    name: 'Fizz',
+    img: 'champs/Fizz.jpg',
+    hair: 'none',
+    costume: 'blue',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['knock up', 'slow']
   },
   {
-    name: 'Jean',
-    img: 'images/jean.svg',
-    hair: 'brown',
-    eyes: 'blue',
-    accessories: ['glasses', 'hat'],
-    other: ['smoker']
-  },
-  {
-    name: 'Jeane',
-    img: 'images/jeane.svg',
-    hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
-  },
-  {
-    name: 'Jed',
-    img: 'images/jed.svg',
-    hair: 'orange',
-    eyes: 'green',
-    accessories: ['glasses', 'hat'],
-    other: ['smoker']
-  },
-  {
-    name: 'Jenni',
-    img: 'images/jenni.svg',
+    name: 'Riven',
+    img: 'champs/Riven.jpg',
     hair: 'white',
-    eyes: 'hidden',
-    accessories: ['hat'],
-    other: []
+    costume: 'white',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['knock back', 'stunn']
   },
   {
-    name: 'Jeri',
-    img: 'images/jeri.svg',
-    hair: 'orange',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
-  },
-  {
-    name: 'Jerry',
-    img: 'images/jerry.svg',
-    hair: 'hidden',
-    eyes: 'blue',
-    accessories: ['hat'],
-    other: []
-  },
-  {
-    name: 'Jess',
-    img: 'images/jess.svg',
+    name: 'Kayn',
+    img: 'champs/Kayn.jpg',
     hair: 'black',
-    eyes: 'blue',
-    accessories: ['glasses'],
-    other: []
+    costume: 'black',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['slow']
   },
   {
-    name: 'Jocelyn',
-    img: 'images/jocelyn.svg',
-    hair: 'black',
-    eyes: 'brown',
-    accessories: ['glasses'],
-    other: []
-  },
-  {
-    name: 'Jon',
-    img: 'images/jon.svg',
+    name: 'Graves',
+    img: 'champs/Graves.jpg',
     hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    costume: 'red',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['slow', 'knock back', 'blind']
   },
   {
-    name: 'Jordan',
-    img: 'images/jordan.svg',
-    hair: 'yellow',
-    eyes: 'hidden',
-    accessories: ['glasses', 'hat'],
-    other: []
+    name: 'Katarina',
+    img: 'champs/Katarina.jpg',
+    hair: 'red',
+    costume: 'black',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: []
   },
   {
-    name: 'Josephine',
-    img: 'images/josephine.svg',
-    hair: 'grey',
-    eyes: 'brown',
-    accessories: [],
-    other: []
+    name: 'Shaco',
+    img: 'champs/Shaco.jpg',
+    hair: 'hidden',
+    costume: 'red',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['fear', 'slow']
   },
   {
-    name: 'Josh',
-    img: 'images/josh.svg',
-    hair: 'yellow',
-    eyes: 'green',
-    accessories: [],
-    other: []
+    name: 'Tristana',
+    img: 'champs/Tristana.jpg',
+    hair: 'red',
+    costume: 'green',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['slow', 'knock back']
   },
   {
-    name: 'Jude',
-    img: 'images/jude.svg',
+    name: 'Warwick',
+    img: 'champs/Warwick.jpg',
+    hair: 'none',
+    costume: 'brown',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['fear', 'suppress']
+  },
+  {
+    name: 'Darius',
+    img: 'champs/Darius.jpg',
+    hair: 'brown',
+    costume: 'red',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['slow', 'hook']
+  },
+  {
+    name: 'Thresh',
+    img: 'champs/Thresh.jpg',
+    hair: 'none',
+    costume: 'green',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['knock back', 'hook', 'slow']
+  },
+  {
+    name: 'Poppy',
+    img: 'champs/Poppy.jpg',
+    hair: 'white',
+    costume: 'brown',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['knock back', 'grounded', 'slow', 'stunn']
+  },
+  {
+    name: 'Rakan',
+    img: 'champs/Rakan.jpg',
+    hair: 'white',
+    costume: 'red',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['knock up', 'charm']
+  },
+  {
+    name: 'Teemo',
+    img: 'champs/Teemo.jpg',
+    hair: 'purple',
+    costume: 'brown',
+    abilities: ['attack damage', 'ability power', 'passive'],
+    crowdcontrol: ['blind', 'slow']
+  },
+  {
+    name: 'Nidalee',
+    img: 'champs/Nidalee.jpg',
+    hair: 'brown',
+    costume: 'brown',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['root']
+  },
+  {
+    name: 'Mordekaiser',
+    img: 'champs/Mordekaiser.jpg',
+    hair: 'hidden',
+    costume: 'black',
+    abilities: ['attack damage', 'ability power', 'passive'],
+    crowdcontrol: ['hook']
+  },
+  {
+    name: 'Caitlyn',
+    img: 'champs/Caitlyn.jpg',
     hair: 'black',
-    eyes: 'green',
-    accessories: [],
-    other: []
+    costume: 'purple',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['root', 'slow']
   },
   {
-    name: 'Julie',
-    img: 'images/julie.svg',
+    name: 'Lulu',
+    img: 'champs/Lulu.jpg',
+    hair: 'purple',
+    costume: 'red',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['knock up', 'slow']
+  },
+  {
+    name: 'Qiyana',
+    img: 'champs/Qiyana.jpg',
+    hair: 'white',
+    costume: 'blue',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['stunn']
+  },
+  {
+    name: 'Garen',
+    img: 'champs/Garen.jpg',
+    hair: 'brown',
+    costume: 'blue',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['silence']
+  },
+  {
+    name: 'Miss Fortune',
+    img: 'champs/MissFortune.jpg',
+    hair: 'red',
+    costume: 'white',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['slow']
+  },
+  {
+    name: 'Akali',
+    img: 'champs/Akali.jpg',
     hair: 'black',
-    eyes: 'brown',
-    accessories: ['glasses', 'hat'],
-    other: []
+    costume: 'green',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: []
+  },
+  {
+    name: 'Gwen',
+    img: 'champs/Gwen.jpg',
+    hair: 'blue',
+    costume: 'white',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['slow']
+  },
+  {
+    name: 'Singed',
+    img: 'champs/Singed.jpg',
+    hair: 'none',
+    costume: 'pink',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['grounded', 'slow', 'root']
+  },
+  {
+    name: 'Jinx',
+    img: 'champs/Jinx.jpg',
+    hair: 'blue',
+    costume: 'pink',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['slow', 'root']
+  },
+  {
+    name: 'Shen',
+    img: 'champs/Shen.jpg',
+    hair: 'hidden',
+    costume: 'purple',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['taunt', 'slow']
+  },
+  {
+    name: 'Jarvan',
+    img: 'champs/Jarvan.jpg',
+    hair: 'hidden',
+    costume: 'gold',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['knock up']
+  },
+  {
+    name: 'Seraphine',
+    img: 'champs/Seraphine.jpg',
+    hair: 'pink',
+    costume: 'purple',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['charm']
+  },
+  {
+    name: 'Rammus',
+    img: 'champs/Rammus.jpg',
+    hair: 'hidden',
+    costume: 'green',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['knock up', 'taunt', 'slow']
+  },
+  {
+    name: 'Sona',
+    img: 'champs/Sona.jpg',
+    hair: 'blue',
+    costume: 'blue',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['stunn']
+  },
+  {
+    name: 'Sett',
+    img: 'champs/Sett.jpg',
+    hair: 'pink',
+    costume: 'white',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: ['stunn', 'slow', 'suppress']
+  },
+  {
+    name: 'Sivir',
+    img: 'champs/Sivir.jpg',
+    hair: 'brown',
+    costume: 'gold',
+    abilities: ['attack damage', 'passive'],
+    crowdcontrol: [ ]
+  },
+  {
+    name: 'Zyra',
+    img: 'champs/Zyra.jpg',
+    hair: 'pink',
+    costume: 'green',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['knock up', 'root']
+  },
+  {
+    name: 'Blitzcrank',
+    img: 'champs/Blitzcrank.jpg',
+    hair: 'none',
+    costume: 'gold',
+    abilities: ['ability power', 'passive'],
+    crowdcontrol: ['knock up', 'hook', 'silence']
   },
 ]
 
@@ -267,14 +411,14 @@ const checkQuestion = () => {
   if (category === 'hair') {
   keep = (secret.hair === value)
   
-  } else if (category === 'eyes') {
-  keep = (secret.eyes === value)
+  } else if (category === 'costume') {
+  keep = (secret.costume === value)
 
-  } else if (category === 'accessories') {
-    keep = (secret.accessories.includes(value))
+  } else if (category === 'abilities') {
+    keep = (secret.abilities.includes(value))
 
-  } else if (category === 'others') {
-    keep = (secret.other.includes(value))
+  } else if (category === 'crowdcontrol') {
+    keep = (secret.crowdcontrol.includes(value))
   }
 
   filterCharacters(keep)
@@ -285,55 +429,50 @@ const checkQuestion = () => {
 const filterCharacters = (keep) => {
   const { category, value } = currentQuestion
   // Show the correct alert message for different categories
-  if (category === 'accessories') {
+  if (category === 'abilities') {
     if (keep) {
-      alert(`Yes, the person have ${value} accessories! Keep all people that have ${value} accessories.`)
+      alert(`Yes, the champion have ${value} abilities! Keep all champion that have ${value} abilities.`)
       charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value))
     } else {
-      alert(
-        `No, the person doesn't have ${value} accessories! Remove all people that have ${value} accessories`)
+      alert(`No, the champion doesn't have ${value} abilities! Remove all champions that have ${value} abilities`)
         charactersInPlay = charactersInPlay.filter((person) => !person[category].includes(value))
     }
-  } else if (category === 'other') {
+  } else if (category === 'crowdcontrol') {
     if (keep) {
-      alert(`Yes, the person have ${value} other! Keep all people that have ${value} other.`)
+      alert(`Yes, the champion have ${value} as crowd control! Keep all champions that have ${value} as crowd control.`)
       charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value))
     } else {
-      alert(`No, the person doesn't have ${value} other! Remove all people that have ${value} other`)
+      alert(`No, the champion doesn't have ${value} as crowd control! Remove all champions that have ${value} as crowd control`)
       charactersInPlay = charactersInPlay.filter((person) => !person[category].includes(value))
     }
 
-  } else {
+  } else if (category === 'hair') {
     if (keep) {
-      alert(`Yes, the person wears ${value} hair! Keep all people that wears ${value} hair`)
+      alert(`Yes, the champion has ${value} hair! Keep all champions with ${value} hair`)
       charactersInPlay = charactersInPlay.filter((person) => person[category] === value)
     } else {
-      alert(`No, the person doesn't have ${value} hair/eyes! Remove all people that have ${value} hair/eyes`)
+      alert(`No, the champion doesn't have ${value} hair! Remove all champions that have ${value} hair`)
       charactersInPlay = charactersInPlay.filter((person) => person[category] !== value)
+      }
+     } else if(category === 'costume') {
+      if (keep) {
+        alert(`Yes, the champion has ${value} costume! Keep all champions with ${value} costume`)
+        charactersInPlay = charactersInPlay.filter((person) => person[category] === value)
+      } else {
+        alert(`No, the champion doesn't have ${value} costume! Remove all champions that have ${value} costume`)
+        charactersInPlay = charactersInPlay.filter((person) => person[category] !== value)
+      }
     }
+
+    //Makes the bord redraw with remaining champions
+    generateBoard()
   }
 
-  // Determine what is the category
-  // filter by category to keep or remove based on the keep variable.
-  /* 
-    for hair and eyes :
-      charactersInPlay = charactersInPlay.filter((person) => person[attribute] === value)
-      or
-      charactersInPlay = charactersInPlay.filter((person) => person[attribute] !== value)
 
-    for accessories and other
-      charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value))
-      or
-      charactersInPlay = charactersInPlay.filter((person) => !person[category].includes(value))
-  */
-
-  // Invoke a function to redraw the board with the remaining people.
-generateBoard()
-}
 // when clicking guess, the player first have to confirm that they want to make a guess.
 const guess = (suspectCharacter) => {
   // store the interaction from the player in a variable.
-  const confirmGuess = confirm(`Is this person your guess?`)
+  const confirmGuess = confirm(`Is this champion your guess?`)
   if (confirmGuess)
   checkMyGuess(suspectCharacter)
   // remember the confirm() ?
@@ -344,9 +483,9 @@ const guess = (suspectCharacter) => {
 const checkMyGuess = (suspectCharacter) => {
 
   if (suspectCharacter === secret.name) {
-  winOrLoseText.innerHTML = `You won the game! You guessed on ${suspectCharacter} which is the right person`
+  winOrLoseText.innerHTML = `Victory! You guessed on ${suspectCharacter} which is the right champion!`
 } else {
-  winOrLoseText.innerHTML = `You lost! The right person was ${secret.name}...`
+  winOrLoseText.innerHTML = `Defeat! The right champion was ${secret.name}...`
 }
   
   winOrLose.style.display = 'flex';

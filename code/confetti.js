e4759dbda2ff681a22ac62354ca1635029beac32
Darya <darya.lapata@gmail.com>
2021-09-17 14:35:57 +0200

    added info to read me

--- code/confetti.js
+++ code/confetti.js
@@ -1,4 +1,9 @@
 
+// THIS CODE I DONT QUIT UNDERSTAND BUT IT MAKES CONFETTI RAIN :)
+
+
+
+
 var confetti = {
 	maxCount: 150,		//set max confetti count
 	speed: 2,			//set the particle animation speed

1ded8b86cb21e1a5f5b1ad62c9aea65fce945938
Fredrik Holm <karlfredrikholm@gmail.com>
2022-08-29 15:04:06 +0200

    added more options to the HTML

--- code/script.js
+++ code/script.js
@@ -231,7 +231,10 @@ const setSecret = () => {
 const start = () => {
   // Here we're setting charactersInPlay array to be all the characters to start with
   charactersInPlay = CHARACTERS
+
   // What else should happen when we start the game?
+  setSecret();
+  setTimeout(generateBoard, 500);
 }
 
 // setting the currentQuestion object when you select something in the dropdown

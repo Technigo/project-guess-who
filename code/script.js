c81c84417663698735df15129b16ba9e161e7fd5
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-08-30 13:25:52 +0200

    step 2, setting the secret character done

--- code/script.js
+++ code/script.js
@@ -201,7 +201,7 @@ const CHARACTERS = [
 ]
 
 // Global variables
-let secret
+let secret 
 let currentQuestion
 let charactersInPlay
 
@@ -225,13 +225,13 @@ const generateBoard = () => {
 // Randomly select a person from the characters array and set as the value of the variable called secret
 const setSecret = () => {
   secret = charactersInPlay[Math.floor(Math.random() * charactersInPlay.length)]
+  console.log('secret person;', secret) //This console.log allows me to see who the secret person is for each game. 
 }
 
 // This function to start (and restart) the game
 const start = () => {
   // Here we're setting charactersInPlay array to be all the characters to start with
   charactersInPlay = CHARACTERS
-  // What else should happen when we start the game?
 }
 
 // setting the currentQuestion object when you select something in the dropdown
@@ -321,6 +321,7 @@ const checkMyGuess = (personToCheck) => {
 // Invokes the start function when website is loaded
 start()
 generateBoard()
+setSecret()
 
 // All the event listeners
 restartButton.addEventListener('click', start)

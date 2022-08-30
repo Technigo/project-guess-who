f1c7591c49d24ed7f5adf1e6cd19f14ea4e5580e
Jessika Lind <jessika@MacBook-Air-som-tillhor-Jessika.local>
2022-08-30 19:24:17 +0200

    part 3 almost done + more comments in the code

--- code/script.js
+++ code/script.js
@@ -2,6 +2,7 @@
 const board = document.getElementById('board') // a class within the section where the charachters will show. 
 const questions = document.getElementById('questions') // where you do your guesse (the roll-down input section)
 const restartButton = document.getElementById('restart') // the restart button
+const findOutButton = document.getElementById('filter') // the find-out button
 
 // Array with all the characters, as objects
 const CHARACTERS = [
@@ -230,27 +231,59 @@ const setSecret = () => {
 
 // This function to start (and restart) the game
 const start = () => {
-  // Here we're setting charactersInPlay array to be all the characters to start with
-  charactersInPlay = CHARACTERS
+  charactersInPlay = CHARACTERS // Here we're setting charactersInPlay array to be all the characters to start with
 }
 
 // setting the currentQuestion object when you select something in the dropdown
 const selectQuestion = () => {
+  console.log('This is the question that I want answered')
   const category = questions.options[questions.selectedIndex].parentNode.label
-
   // This variable stores what option group (category) the question belongs to.
-  // We also need a variable that stores the actual value of the question we've selected.
-  // const value =
+  // We also need a variable that stores the actual value of the question we've selected in the dropdown 👇
+  const value = questions.value
+    if (category === 'hair') {
+      console.log('hair:', value)
+      currentQuestion = { //this is an object
+        category: category,
+        value: value
+      }
+    }
+  
+    else if (category === 'eyes') {
+      console.log('eyes:', value)
+      currentQuestion = { //this is an object
+        category: category,
+        value: value
+      }
+    }
 
+    else if (category === 'accessories') {
+      console.log('accessories:', value)
+      currentQuestion = { //this is an object
+        category: category,
+        value: value
+      }
+    }
+
+    else if (category === 'other') {
+      console.log('other:', value)
+      currentQuestion = {
+        category: category,
+        value: value
+      }
+     }
+  }
+/* I have now inserted this into the if-elses above. 
   currentQuestion = {
     category: category,
     // value: value
   }
-}
+  */
+
 
 // This function should be invoked when you click on 'Find Out' button.
 const checkQuestion = () => {
-  const { category, value } = currentQuestion
+  const { category, value } = currentQuestion // I wonder why the curly brackets have been used in this way here? 
 
   // Compare the currentQuestion details with the secret person details in a different manner based on category (hair/eyes or accessories/others).
   // See if we should keep or remove people based on that
@@ -312,16 +345,41 @@ const guess = (personToConfirm) => {
 
 // If you confirm, this function is invoked
 const checkMyGuess = (personToCheck) => {
+  /*Here I could put my count down(or up) if-statement. Perhaps look something like this? (First I need to define count) 
+  if (count === 0) {
+    alert('Game over! Refresh the page to try again.')
+  }*/
+  
+  if (personToCheck === personToConfim) {
+    alert('Are you Sherlock Holmes? Cause that sure was impressive detective work!')
+  }
+  else {
+    alert('Not the sharpest tool in the tool box? Give it another go!')
+    
+  }
   // 1. Check if the personToCheck is the same as the secret person's name
   // 2. Set a Message to show in the win or lose section accordingly
   // 3. Show the win or lose section
   // 4. Hide the game board
 }
 
-// Invokes the start function when website is loaded
-start()
-generateBoard()
-setSecret()
 
-// All the event listeners
+start() // Invokes the start function when website is loaded
+generateBoard() // Invokes the generateBoard function when the website is loaded
+setSecret() // Invokes the setSecrets function when website is loaded (i.e. a new secret person is chosen)
+
+// ALL EVENTLISTENERS
+//This eventListener makes sure that the page relodes when the button Restart is clicked. 
 restartButton.addEventListener('click', start)
+
+//This eventListener is activeted when the dropdown is changed. I am not sure that it is needed, and what it should be connected to. 
+questions.addEventListener('change', (event) => {
+  console.log('This might be my guess')
+  selectQuestion()
+}) 
+
+//This eventListener is supposed to check whether my question is right or not (thus activate an alert message). I have not made it work yet.
+findOutButton.addEventListener('click', (event) => {
+  console.log('I choose this one!')
+  checkQuestion()
+})

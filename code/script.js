5c709e8340510264d003414fa3dd72889b753b42
fannystenberg <ffannystenberg@gmail.com>
2023-02-16 13:12:43 +0100

    reduced filtering to less code with if/else

--- code/script.js
+++ code/script.js
@@ -282,38 +282,35 @@ const checkQuestion = () => {
 const filterCharacters = (keep) => {
   const { category, value } = currentQuestion
   // Show the correct alert message for different categories
-  // filter by category to keep or remove based on the keep variable.
   if (category === 'accessories') {
     if (keep) {
       alert(`Yes, the person wears ${value}! Keep all people that wears ${value}`)
-      charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value))
     } else {
       alert(`No, the person doesn't wear ${value}! Remove all people that wears ${value}`)
-      charactersInPlay = charactersInPlay.filter((person) => !person[category].includes(value))
     }
 
   } else if (category === 'other') {
-    // Similar to the one above
     if (keep) {
       alert(`Yes, the person is a ${value}! Keep all that are ${value}s`)
-      charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value))
     } else {
       alert(`No, the person isn't a ${value}! Remove all that aren't ${value}s`)
-      charactersInPlay = charactersInPlay.filter((person) => !person[category].includes(value))
     }
 
   } else if (category === 'hair' || category === 'eyes') {
     if (keep) {
-      // alert popup that says something like: "Yes, the person has yellow hair! Keep all people with yellow hair"
       alert(`Yes, the person has ${value} ${category}! Keep all people with ${value} ${category}`)
-      charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value))
     } else {
-      // alert popup that says something like: "No, the person doesnt have yellow hair! Remove all people with yellow hair"
       alert(`No, the person doesn't have ${value} ${category}! Remove all peole with ${value} ${category}`)
-      charactersInPlay = charactersInPlay.filter((person) => !person[category].includes(value))
     }
   }
 
+  // filter to keep or remove based on the keep variable.
+  if (keep) {
+    charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value))
+  } else {
+    charactersInPlay = charactersInPlay.filter((person) => !person[category].includes(value))
+  }
+  
   // Invoke a function to redraw the board with the remaining people.
   generateBoard()
 }

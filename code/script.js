fdc000ec6d608bd5a9418356cd1550d388bda249
JosefinRobertsson <josefinrobertsson@hotmail.com>
2023-02-19 16:51:23 +0100

    turned secret to random

--- code/script.js
+++ code/script.js
@@ -236,9 +236,7 @@ const generateBoard = () => {
 
 // Randomly select a person from the characters array and set as the value of the variable called secret
 const setSecret = () => {
-  secret =
-    charactersInPlay[20];
-//[Math.floor(Math.random() * charactersInPlay.length)];
+  secret = charactersInPlay[Math.floor(Math.random() * charactersInPlay.length)];
 };
 
 // This function to start (and restart) the game

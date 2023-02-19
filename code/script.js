5756a101589a5b668e23ad8ef0df745e26bbf828
Lina Adamsson <studio@adamsson.com>
2023-02-19 09:22:56 +0100

    Cleaned up comments.

--- code/script.js
+++ code/script.js
@@ -1,4 +1,4 @@
-// All the DOM selectors stored as short variables
+// All the DOM selectors stored as short variables.
 const board = document.getElementById("board");
 const questions = document.getElementById("questions");
 const findOutButton = document.getElementById("filter");
@@ -201,16 +201,16 @@ const CHARACTERS = [
   },
 ];
 
-// Global variables – will let you know how later
-let secret; // Will be the secret person object
-let currentQuestion; // Will be the current question object
-let charactersInPlay; // Will be an arry of all people left in the game
+// Global variables – will let you know how later on.
+let secret; // Will be the secret person object.
+let currentQuestion; // Will be the current question object.
+let charactersInPlay; // Will be an arry of all people left in the game.
 
-// Draw the game board
+// Draw the game board.
 const generateBoard = () => {
-  board.innerHTML = ""; // 237 when you restart, it clears it innerhtml. The board.
+  board.innerHTML = ""; // When you restart, the innerhtml clears.
   charactersInPlay.forEach((person) => {
-    // To display each person, looping. //adding html to board wrapper.
+    // To display each person, looping. Adding html to board wrapper.
     board.innerHTML += `
       <div class="card">
         <p>${person.name}</p>
@@ -224,49 +224,45 @@ const generateBoard = () => {
   });
 };
 
-// Randomly select a person from the characters array and set as the value of the variable called secret
+// Randomly select a person from the characters array and set as the value of the variable called secret.
 const setSecret = () => {
   secret =
     charactersInPlay[Math.floor(Math.random() * charactersInPlay.length)];
 };
 
-// Here we start to fill up the board
-// This function to start (and restart) the game
+// Here we start to fill up the board.
+// This function, "start", to start (and restart) the game. Here we call other functions.
 const start = () => {
-  //funktion - i vilken vi kallar andra funktionre. När man väcker
-  // Here we're setting charactersInPlay array to be all the characters to start with. Change the value of variable charactersInPlay to CHARCTERS. Holds the value of the whole array.
-  charactersInPlay = CHARACTERS; // hämtar ALLA karaktärerna
-  // call the generate board to make spelplanen visible
+  // Here we're setting charactersInPlay array to be all the characters to start with. Change the value of the variable charactersInPlay to CHARACTERS. Holds the value of the whole array.
+  charactersInPlay = CHARACTERS; // fetches ALL characters.
+  // Call the generate board to make the board visible.
   // What else should happen when we start the game?
-  // The values are global – work wherever
-  generateBoard(); // väcker den = calling the function // later, this function will fetch the filtered characters. Generating new arrays rather than deleting characters. The filteredCharacters variable parameter/argument
+  // The values are global – work wherever.
+  generateBoard(); // "Awakens" the board = calling the function. Later, this function will fetch the filtered characters. Generating new arrays rather than deleting characters – the filteredCharacters variable's parameter/argument.
   setSecret();
   console.log("yyy", secret.name);
 };
 
-// setting the currentQuestion object when you select something in the dropdown
-// a function that needs to be invoked by adding an eventListener
+// Setting the currentQuestion object when you select something in the dropdown. A function that needs to be invoked by adding an eventListener.
 const selectQuestion = () => {
-  const category = questions.options[questions.selectedIndex].parentNode.label; //question är id. category has been defined
+  const category = questions.options[questions.selectedIndex].parentNode.label; //Question = an id. The category has been defined.
   // This variable stores what option group (category) the question belongs to.
   //if === hair // global variable
-
   // We also need a variable that stores the actual value of the question we've selected.
   const value = questions.value;
   console.log("qqq", value)
+  // If we don't change the dropdown, the category and value (category + value) haven't been declared = can't be used = error in console.
+  //selectQuestion = the eventListener is listening to the change in category calling the selectQuestion function.
 
-  // if we don't change the dropdown, the category and value (category + value) hAVEN'T BEEN DEClared = can't be used = error in console
-  //selectQuestion = the eventKistener is listening to the change in category calling the selectQuestion function
-
-  // This turns into if else, the whole conditional
+  // This turns into if else, the whole conditional.
   currentQuestion = {
     category: category,
     value: value,
   };
 
-  // 1st selectQ declares cat and value
-  // 2nd it prepares data to be compared. Not yet doing anything w/ currentData
-  // if U have selected category eyes, this is what CurrentQuestion (GLOBAL) means. We're not re-declaring it, we-re redefining it
+  // At first, the selectQuestion declares category and value.
+  // Then it prepares data to be compared. Not yet doing anything w/ currentData.
+  // If you have selected category eyes, this is what CurrentQuestion (a GLOBAL variable) means. We're not re-declaring it, we-re redefining it.
 
   console.log(
     "new dropdown item has been selected",
@@ -275,39 +271,35 @@ const selectQuestion = () => {
   );
 };
 
-// This function should be invoked when you click on 'Find Out' button. Called by an eventListener. Find Out button!! ett filter
+// This function should be invoked when you click on 'Find Out' button. Called by an eventListener, the Find Out button. A filter.
 const checkQuestion = () => {
   const { category, value } = currentQuestion;
 
-  // If you've asked wetjer the secret person has glasses, it checks weather it does
-
+  // If you've asked whether the secret person has glasses, it checks weather it does.
   // Compare the currentQuestion details with the secret person details in a different manner based on category (hair/eyes or accessories/others).
-  // See if we should keep or remove people based on that
-  // Then invoke filterCharacters
+  // See if we should keep or remove people based on that.
+  // Then invoke filterCharacters.
+  keep = false; // If this line of code is not included, then we'll get wrong answers when selecting values from grouped categories. eg. without keep = false.
+  // If a character has yellow hair and hidden eyes, I ask about yellow hair, the alert says yes they have yellow hair. Then I ask if they have green eyes, alert will give me a false yes, because now keep has been changed to true for hair and eyes.
 
-  keep = false; // if this line of code is not included, then get wrong answers when selecting values from grouped categories. E.g. without keep = false,
-  // if a character has yellow hair and hidden eyes.  I ask about yellow hair, the alert says yes they have yellow hair. Then I ask if they have green eyes, alert will give me a false yes,
-  //because now keep has been changed to true for hair and eyes.
+  // Below: defining what categories and values to keep.
+  // We need to define the two else if conditionals here, values and attributes + arrays.
+  // Here defining keep, what it means. Telling the browser what keep means.
+  // if (category === "hair" || category === "eyes") { // Each object property has one value.
+  // Arrays below, where each object property has an array of values.
+  // } else if (category === "accessories" || category === "other") { // need to be defined differently than hair and eyes.
+  // }
 
-  // below: defining what categories and values to keep
-  // we need to define the two else if conditionals here, values and attributes + arrays
-  // here defining keep, what it means. Telling the browser what keep means
-  if (category === "hair" || category === "eyes") {
-    // each object property has one value
-    // arrays below! where each object property has an array of values
-  } else if (category === "accessories" || category === "other") {
-    // need to be defined differently than hair and eyes
-  }
-
-  // Invoking the function filterCharacters with the varible keep
+  // Invoking the function filterCharacters with the varible keep.
   filterCharacters(keep);
 };
 
-// here using the keep variable set up above
+// Here using the keep variable set up above.
 // It'll filter the characters array and redraw the game board.
+
 const filterCharacters = (keep) => {
   const { category, value } = currentQuestion;
-  // Show the correct alert message for different categories
+  // Show the correct alert message for different categories.
   if (category === "accessories") {
     if (keep) {
       console.log("xxx", category);
@@ -320,25 +312,25 @@ const filterCharacters = (keep) => {
       );
     }
   } else if (category === "other") {
-    // Similar to the one above
+    // Similar to the one above.
   } else {
     if (keep) {
       alert(
-        `Yes, the person is a ${value}! Remove all people that wears ${value}`
+        `Yes, the person is a ${value}! Remove all people that ${value}`
       );
-      // alert popup that says something like: "Yes, the person has yellow hair! Keep all people with yellow hair"
+      // Alert popup that says something like: "Yes, the person has yellow hair! Keep all people with yellow hair".
     } else {
       alert(
-        `No, the person is not a ${value}! Remove all people that wears ${value}`
+        `No, the person is not a ${value}! Remove all people that ${value}`
       );
-      // alert popup that says something like: "No, the person doesnt have yellow hair! Remove all people with yellow hair"
+      // Alert popup that says something like: "No, the person doesnt have yellow hair! Remove all people with yellow hair".
     }
   }
 
-  // now get removal function to work
-  // Determine what is the category
-  // filter by category to keep or remove based on the keep variable.
-  //below starts filtering the board
+  // Now get the removal function to work.
+  // Determine what is the category.
+  // Filter by category to keep or remove based on the keep variable.
+  // Below starts filtering the board.
 
   /* 
     for hair and eyes :
@@ -356,32 +348,31 @@ const filterCharacters = (keep) => {
   generateBoard();
 };
 
-// when clicking guess, the player first have to confirm that they want to make a guess.
+// When clicking guess, the player first have to confirm that they want to make a guess.
 const guess = (personToConfirm) => {
-  // store the interaction from the player in a variable.
-  // remember the confirm() ?
+  // Store the interaction from the player in a variable.
+  // Remember the confirm() ?
   // If the player wants to guess, invoke the checkMyGuess function.
 };
 
-// If you confirm, this function is invoked
+// If you confirm, this function is invoked.
 const checkMyGuess = (personToCheck) => {
-  // 1. Check if the personToCheck is the same as the secret person's name
-  // 2. Set a Message to show in the win or lose section accordingly
-  // 3. Show the win or lose section
-  // 4. Hide the game board
+  // 1. Check if the personToCheck is the same as the secret person's name.
+  // 2. Set a Message to show in the win or lose section accordingly.
+  // 3. Show the win or lose section.
+  // 4. Hide the game board.
 };
 
-// Invokes the start function when website is loaded
+// Invokes the start function when website is loaded.
 start();
 
-// All the event listeners //in the html files
-restartButton.addEventListener("click", start); // listening for button
-questions.addEventListener("change", selectQuestion); //dropdwon menu asking questions, listening to questions. The event is on questions, and the Listener is calling the selectQuestions which is a function that needs to be populated
+// All the event listeners in the html files.
+restartButton.addEventListener("click", start); // Listening for button.
+questions.addEventListener("change", selectQuestion); // Dropdwon menu asking questions, listening to questions. The event is on questions, and the Listener is calling the selectQuestions which is a function that needs to be populated.
 findOutButton.addEventListener("click", () => {
-  // paranthesis and arrow invoke another happening
+  // Paranthesis and arrow invoke another happening.
   checkQuestion();
-  filterCharacters();
 });
-//Eventlisteners can be in the bottom if it works all the time
+//Eventlisteners can be in the bottom if it works all the time.
 
-// Put in function, call it once =
+// Put in function, call it once.

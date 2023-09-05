012962d3f3ab9f975f8310f19ead2ae7e7d3327f
Your Name <you@example.com>
2023-09-05 18:29:30 +0200

    step 1

--- code/script.js
+++ code/script.js
@@ -231,7 +231,9 @@ const setSecret = () => {
 const start = () => {
   // Here we're setting charactersInPlay array to be all the characters to start with
   charactersInPlay = CHARACTERS
-  // What else should happen when we start the game?
+  
+  //Invoke the board
+  generateBoard();
 }
 
 // setting the currentQuestion object when you select something in the dropdown

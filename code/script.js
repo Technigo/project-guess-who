4e5263f8c5c45de6131d47d53eac4ae369a54250
Breanna Sand <breanna.sand8@gmail.com>
2023-09-06 12:51:10 +0200

    test commit

--- code/script.js
+++ code/script.js
@@ -3,6 +3,8 @@ const board = document.getElementById('board')
 const questions = document.getElementById('questions')
 const restartButton = document.getElementById('restart')
 
+//First commit 
+
 // Array with all the characters, as objects
 const CHARACTERS = [
   {

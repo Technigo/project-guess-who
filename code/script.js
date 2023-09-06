6a4525d58fc2437c1d49f9eadfd33ddee8fe63fa
Susanne Ekenheim <susanne.ekenheim@gmail.com>
2023-09-06 20:35:11 +0200

    declared the 'keep'-variable updated the 'checkQuestion'-function

--- code/script.js
+++ code/script.js
@@ -248,8 +248,12 @@ const selectQuestion = () => {
 
   // This variable stores what option group (category) the question belongs to.
   // We also need a variable that stores the actual value of the question we've selected.
-  const value = selectedOption.value;
+  
   const selectedOption = questions.options[questions.selectedIndex];
+  const value = selectedOption.value;
+
+  console.log(category)
+  console.log(value)
   
   currentQuestion = {
     category: category,
@@ -259,16 +263,23 @@ const selectQuestion = () => {
 
 // This function should be invoked when you click on 'Find Out' button.
 const checkQuestion = () => {
-  const { category, value } = currentQuestion
+  const {category, value} = currentQuestion
+  let keep
+  console.log(`category: ${category}`)
+  console.log(`value: ${value}`)
+  console.log(secret)
 
   // Compare the currentQuestion details with the secret person details in a different manner based on category (hair/eyes or accessories/others).
   // See if we should keep or remove people based on that
   // Then invoke filterCharacters
   if (category === 'hair' || category === 'eyes') {
-
+    keep = category === 'hair' ? (secret.hair === value) : (secret.eyes === value)
   } else if (category === 'accessories' || category === 'other') {
-
+    // Defines the condition for 'accessories' or 'other' category.
+    keep = category === 'accessories' ? (secret.accessories === value) : (secret.other === value)
   }
+  console.log(keep)
+  filterCharacters(keep)
 }
 
 // It'll filter the characters array and redraw the game board.
@@ -332,5 +343,5 @@ start()
 
 // All the event listeners
 restartButton.addEventListener('click', start)
-questions.addEventListener('change', currentQuestion)
-//filterButton.addEventListener('change' )
+questions.addEventListener('change', selectQuestion)
+filterButton.addEventListener('click', checkQuestion)

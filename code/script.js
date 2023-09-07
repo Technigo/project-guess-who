265c2360e2b9fb64a14031f6b5feab944fa50232
Amanda Rambert Hammarström <amanda.rambert@gmail.com>
2023-09-07 22:50:33 +0200

    finished start function and added eventlistener for findOut

--- code/script.js
+++ code/script.js
@@ -2,6 +2,7 @@
 const board = document.getElementById('board')
 const questions = document.getElementById('questions')
 const restartButton = document.getElementById('restart')
+const findOutButton = document.getElementById('filter')
 
 // Array with all the characters, as objects
 const CHARACTERS = [
@@ -231,8 +232,13 @@ const setSecret = () => {
 const start = () => {
   // Here we're setting charactersInPlay array to be all the characters to start with
   charactersInPlay = CHARACTERS
-  // Generate the board on start.
+  // The computer chooses a character which the player has to guess.
+  setSecret();
+  // Invoking the function generateBoard to generate the board on start.
   generateBoard();
+
+  // Select the default question as the starting question.
+  selectQuestion();
 }
 
 // setting the currentQuestion object when you select something in the dropdown
@@ -241,23 +247,27 @@ const selectQuestion = () => {
 
   // This variable stores what option group (category) the question belongs to.
   // We also need a variable that stores the actual value of the question we've selected.
-  // const value =
+  const value = questions.value;
 
   currentQuestion = {
     category: category,
-    // value: value
+    value: value
   }
 }
 
 // This function should be invoked when you click on 'Find Out' button.
 const checkQuestion = () => {
   const { category, value } = currentQuestion
-
   // Compare the currentQuestion details with the secret person details in a different manner based on category (hair/eyes or accessories/others).
   // See if we should keep or remove people based on that
   // Then invoke filterCharacters
+  let keep;
   if (category === 'hair' || category === 'eyes') {
-
+    if (category === 'hair') {
+      keep = secret.hair === value
+    } else {
+      keep = secret.eyes === value
+    }
   } else if (category === 'accessories' || category === 'other') {
 
   }
@@ -323,4 +333,7 @@ const checkMyGuess = (personToCheck) => {
 start()
 
 // All the event listeners
-restartButton.addEventListener('click', start)
+restartButton.addEventListener('click', start);
+questions.addEventListener('change', selectQuestion);
+findOutButton.addEventListener('click', checkQuestion);
+

321386644f1e023ae888c39b12303f533510d7ef
elbines <141184135+elbines@users.noreply.github.com>
2023-09-11 23:20:16 +0200

    just started changing everything again, not finished

--- code/script.js
+++ code/script.js
@@ -15,197 +15,244 @@ const winOrLose = document.getElementById('winOrLose');
 // Array with all the characters, as objects
 const CHARACTERS = [
   {
-    name: 'Jabala',
-    img: 'images/jabala.svg',
-    hair: 'hidden',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: ['smile']
+    name: 'You need to calm down',
+    img: 'images/YNTCD.jpeg',
+    versions: 'new',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jack',
-    img: 'images/jack.svg',
-    hair: 'hidden',
-    eyes: 'blue',
-    accessories: ['hat'],
-    other: ['beard']
+    name: 'Mean',
+    img: 'images/mean.jpg',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jacques',
-    img: 'images/jacques.svg',
-    hair: 'hidden',
-    eyes: 'blue',
-    accessories: [],
-    other: ['smoker', 'beard']
+    name: 'Cornelia street',
+    img: 'images/corneliastreet.png',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jai',
-    img: 'images/jai.svg',
-    hair: 'black',
-    eyes: 'brown',
-    accessories: [],
-    other: ['smile']
+    name: 'The Man',
+    img: 'images/THEMAN.png',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jake',
-    img: 'images/jake.svg',
-    hair: 'blonde',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: ['smile']
+    name: 'Look what you made me do',
+    img: 'images/LWYMMD.jpg',
+    versions: 'taylors',
+    erastour: 'setlist',
+    cinematic: 'musicviideo',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'James',
-    img: 'images/james.svg',
-    hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: 'Delicate',
+    img: 'images/Delicate.png',
+    versions: 'stolen',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jana',
-    img: 'images/jana.svg',
-    hair: 'black',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: ['smile']
+    name: 'Ready for it',
+    img: 'images/readyforit.png',
+    versions: 'new',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jane',
-    img: 'images/jane.svg',
-    hair: 'blonde',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: []
+    name: 'Anti-hero',
+    img: 'images/antihero.jpg',
+    versions: 'taylors',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jaqueline',
-    img: 'images/jaqueline.svg',
-    hair: 'orange',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: ['smile']
-  },
-
-  {
-    name: 'Jazebelle',
-    img: 'images/jazebelle.svg',
-    hair: 'purple',
-    eyes: 'hidden',
-    accessories: ['glasses'],
-    other: ['smoker']
+    name: 'Cruel summer',
+    img: 'images/cruelsummer.jpg',
+    versions: 'orange',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jean',
-    img: 'images/jean.svg',
-    hair: 'brown',
-    eyes: 'blue',
-    accessories: ['glasses', 'hat'],
-    other: ['smoker']
+    name: 'WANEGBT',
+    img: 'images/WANEGBT.jpg',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jeane',
-    img: 'images/jeane.svg',
-    hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: ['smile']
+    name: 'You’re on your own kid',
+    img: 'images/YOYOK.jpg',
+    versions: 'stolen',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['hearthbreak', 'breakup']
   },
   {
-    name: 'Jed',
-    img: 'images/jed.svg',
-    hair: 'red',
-    eyes: 'green',
-    accessories: ['glasses', 'hat'],
-    other: ['smoker', 'beard']
+    name: 'All too well',
+    img: 'images/alltoowell.jpg',
+    versions: 'stolen',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship'],
   },
   {
-    name: 'Jenni',
-    img: 'images/jenni.svg',
-    hair: 'white',
-    eyes: 'hidden',
-    accessories: ['hat'],
-    other: []
+    name: 'Midnight rain',
+    img: 'images/midnightrain.jpg',
+    versions: 'stolen',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Jeri',
-    img: 'images/jeri.svg',
-    hair: 'red',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: []
+    name: 'I bet you think about me ',
+    img: 'images/IBYTAM.jpg',
+    versions: 'new',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Jerry',
-    img: 'images/jerry.svg',
-    hair: 'hidden',
-    eyes: 'blue',
-    accessories: ['hat'],
-    other: ['smile']
+    name: 'Fearless',
+    img: 'images/fearless.png',
+    versions: 'stolen',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Jess',
-    img: 'images/jess.svg',
-    hair: 'black',
-    eyes: 'blue',
-    accessories: ['glasses'],
-    other: ['smile']
+    name: 'Should’ve said no',
+    img: 'images/shouldvesaidno.png',
+    versions: 'new',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Jocelyn',
-    img: 'images/jocelyn.svg',
-    hair: 'black',
-    eyes: 'brown',
-    accessories: ['glasses', 'earrings'],
-    other: []
+    name: 'Right where you left me',
+    img: 'images/RWYLM.jpg',
+    versions: 'new',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Jon',
-    img: 'images/jon.svg',
-    hair: 'brown',
-    eyes: 'green',
-    accessories: ['glasses'],
-    other: ['smile']
+    name: 'Cowboy like me',
+    img: 'images/cowboylikeme.jpg',
+    versions: 'new',
+    erastour: 'setlist',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Jordan',
-    img: 'images/jordan.svg',
-    hair: 'blonde',
-    eyes: 'hidden',
-    accessories: ['glasses', 'hat'],
-    other: ['smile']
+    name: 'Tolerate it',//!!
+    img: 'images/tolerateit.jpg',
+    versions: 'stolen',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Josephine',
-    img: 'images/josephine.svg',
-    hair: 'grey',
-    eyes: 'brown',
-    accessories: ['earings'],
-    other: ['smile']
+    name: 'Blank space',
+    img: 'images/blankspace.png',
+    versions: 'taylors',
+    erastour: 'surprise',
+    cinematic: 'music video',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Josh',
-    img: 'images/josh.svg',
-    hair: 'blonde',
-    eyes: 'green',
-    accessories: [],
-    other: ['smile']
+    name: 'Bad blood',
+    img: 'images/badblood.jpg',
+    versions: 'taylors',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Jude',
-    img: 'images/jude.svg',
-    hair: 'black',
-    eyes: 'green',
-    accessories: [],
-    other: ['smile', 'beard']
+    name: 'Mirrorball',
+    img: 'images/mirrorball.jpg',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
   },
   {
-    name: 'Julie',
-    img: 'images/julie.svg',
-    hair: 'black',
-    eyes: 'brown',
-    accessories: ['glasses', 'hat'],
-    other: []
+    name: 'I know places',
+    img: 'images/iknowplaces.jpg',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
+  },
+  {
+    name: 'Love story',
+    img: 'images/lovestory.png',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
+  },
+  {
+    name: 'Electric touch',
+    img: 'images/eletrictouch.jpg',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
+  },
+  {
+    name: 'Mad woman',
+    img: 'images/madwoman.png',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
+  },
+  {
+    name: 'Illicit affairs',
+    img: 'images/illicitaffair.jpg',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
+  },
+  {
+    name: 'Maroon',
+    img: 'images/maroon.jpg',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
+  },
+  {
+    name: 'Long live',
+    img: 'images/longlive.jpg',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
+  },
+  {
+    name: 'Champagne problems',//!!
+    img: 'images/champagneproblems.png',
+    versions: 'new',
+    erastour: 'surprise',
+    cinematic: 'no musicvideo',
+    songtheme: ['lifelessons', 'relationship']
   },
 ]
 
@@ -214,6 +261,7 @@ let secret //Will be the secret person object (Stores secret person selected in 
 let currentQuestion //Will be the current question object 
 let charactersInPlay //Will be an array of all the people left in the game 
 let personToConfirm;
+let textForAlert//S
 
 // Draw the game board 
 const generateBoard = () => {
@@ -251,7 +299,6 @@ const start = () => {
   // Here we're setting charactersInPlay array to be all of the characters to start with
   charactersInPlay = CHARACTERS
   // Here I can add if anything else should happen when we start the game?
-
   winOrLose.style.display = 'none'; //Got help with this during demo from the strawberry team :)
   setSecret(); //Secret person selected (let secret, const setSecret = () => {)
   generateBoard();// Board made visible. 
@@ -259,21 +306,24 @@ const start = () => {
 }
 
 
-
-
+// This variable (catagory) stores what option group (category) the question belongs to.
+// Variable that stores the actual value of the question we've selected.-->
+//Careful to not mistake value attribute of the <option> element with text content (or label) of the <option> element on const CHARACTERS.
 // Setting the currentQuestion object when selecting something in the dropdown?
 const selectQuestion = () => {
   const category = questions.options[questions.selectedIndex].parentNode.label
-  // This variable (catagory) stores what option group (category) the question belongs to.
-  // Variable that stores the actual value of the question we've selected.-->z
-  const value = questions.options[questions.selectedIndex].value;
-  //Careful to not mistake value attribute of the <option> element with text content (or label) of the <option> element on const CHARACTERS.
+  const value = questions.options[questions.selectedIndex].value//S
+  textForAlert = questions.options[questions.selectedIndex].text;//S
+
 
   currentQuestion = {
     category: category,
     value: value,
+    textForAlert: textForAlert, //S
   }
 }
+
+//Sakura Tanaka helped me figure out the textForAlert. I couldn't figure it out, but she looked over my code and gave me theese suggestions. 
 
 // This function should be invoked when you click on 'Find Out' button.
 const checkQuestion = () => {
@@ -288,7 +338,7 @@ const checkQuestion = () => {
 
   keep = true;
 
-  if (category === 'hair' || category === 'eyes') {
+  if (category === 'versions' || category === 'erastour') {
     console.log(category, value);
     if (secret[category] === value) //I think this condition checks if the value is exactly the same as secret catergory
     {
@@ -297,7 +347,7 @@ const checkQuestion = () => {
       filterCharacters(false);
     }
   }
-  else if (category === 'accessories' || category === 'other') {
+  else if (category === 'cinematic' || category === 'songtheme') {
     console.log(category, value);
     if (secret[category].includes(value)) ////I think this condition checks if the value is sone of the elements inside the array i think?
     {
@@ -331,48 +381,51 @@ const checkQuestion = () => {
 
 // this should filter the characters and redraw the game board?
 const filterCharacters = (keep) => {
-  const { category, value } = currentQuestion;
+  const { category, value, textForAlert } = currentQuestion;//S
+  //const { category, value } = currentQuestion;
   let message;
+
+
 
   // alert message for different categories.  
   //I originally wanted different messages for each category (its the same now) that is why the code isn't more comact. Not sure if I'll keep it like this.
-  if (category === 'accessories') {
+  if (category === 'cinematic') {
     message = keep
-      ? `Yes, the person wears ${value}! Keep all people that wear ${value}`
-      : `No, the person doesn't wear ${value}! Remove all people that wear ${value}`;
-  } else if (category === 'other') {
+      ? `Yes, the person wears ${textForAlert}! Keep all people that wear ${textForAlert}`
+      : `No, the person doesn't wear ${textForAlert}! Remove all people that wear ${textForAlert}`;
+  } else if (category === 'songtheme') {
     message = keep
-      ? `Yes, the person wears ${value}! Keep all people that wear ${value}`
-      : `No, the person doesn't wear ${value}! Remove all people that wear ${value}`;
-  } else if (category === 'hair') {
+      ? `Yes, the person wears ${textForAlert}! Keep all people that wear ${textForAlert}`
+      : `No, the person doesn't wear ${textForAlert}! Remove all people that wear ${textForAlert}`;
+  } else if (category === 'versions') {
     message = keep
-      ? `Yes, the person has ${value} hair! Keep all people with ${value} hair`
-      : `No, the person doesn't have ${value} hair! Remove all people with ${value} hair`;
-  } else if (category === 'eyes') {
+      ? `Yes, the person has ${textForAlert} hair! Keep all people with ${textForAlert} hair`
+      : `No, the person doesn't have ${textForAlert} hair! Remove all people with ${textForAlert} hair`;
+  } else if (category === 'erastour') {
     message = keep
-      ? `Yes, the person has ${value} eyes! Keep all people with ${value} eyes`
-      : `No, the person doesn't have ${value} eyes! Remove all people with ${value} eyes`;
+      ? `Yes, the person has ${textForAlert} eyes! Keep all people with ${textForAlert} eyes`
+      : `No, the person doesn't have ${textForAlert} eyes! Remove all people with ${textForAlert} eyes`;
   } else {
     message = keep
-      ? `Yes, the person has ${value}! Keep all people with ${value}`
-      : `No, the person doesn't have ${value}! Remove all people with ${value}`;
+      ? `Yes, the person has ${textForAlert}! Keep all people with ${textForAlert}`
+      : `No, the person doesn't have ${textForAlert}! Remove all people with ${textForAlert}`;
   }
 
   alert(message);
 
   // Filtering the characters based on the category and value. 
   //I struggeled with the logic of this part, and where to put it. I think there should be a way to write it to make to code more comact?
-  if (category === 'accessories') {
+  if (category === 'cinematic') {
     charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value) === keep);
-  } else if (category === 'other') {
+  } else if (category === 'songtheme') {
     charactersInPlay = charactersInPlay.filter((person) => person[category].includes(value) === keep);
-  } else if (category === 'hair' || category === 'eyes') {
+  } else if (category === 'versions' || category === 'erastour') {
     charactersInPlay = charactersInPlay.filter((person) => person[category] === value === keep);
   } else {
     // Handle other categories here
   }
 
-  generateBoard(); // Updates the game-board with filtered characters. I couln't make it work without this. 
+  generateBoard();  //Updates the game-board with filtered characters. I couln't make it work without this. 
 }
 
 // when clicking guess, the player first have to confirm that they want to make a guess.
